9ffc8f16dd79e0a00315755709b2f6a055f9a79d
Pedro Arevalo <devpedroarevalo@gmail.com>
2021-09-13 18:44:34 -0400

    added programming related to the date

---
+++ script/script.js
@@ -0,0 +1,37 @@
+var weekDay = new Date().getDay()
+var hour = new Date().getHours()
+
+
+weekDay = 2;
+hour = 9;
+
+var oneTitle = document.querySelector('section#one .sectTitle')
+var oneNotice = document.querySelector('.aviso')
+oneTitle.innerText = ''
+oneNotice.innerText = ''
+$('#menu').hide()
+if (weekDay >=1 && weekDay < 6) {
+    if (hour < 14) {
+       oneTitle.innerText = 'Cardápio de hoje:'
+       $('#menu').show()
+        //CARDÁPIO
+    } else {
+        oneTitle.innerText = 'Até amanhã!'
+        oneNotice.innerHTML = 'Nosso estabelecimento encontra-se <span class="foco">fechado</span> no momento.'
+        //HASTA MAÑANA
+    }
+} else if (weekDay == 6) {
+    if (hour < 14) {
+        oneTitle.innerText = 'Cardápio de hoje:'
+        $('#menu').show()
+        //CARDÁPIO
+    } else {
+        oneTitle.innerText = 'Até segunda!'
+        oneNotice.innerHTML = 'Nosso estabelecimento encontra-se <span class="foco">fechado</span> no momento.'
+        //HASTA EL LUNES
+    }
+} else {
+    oneTitle.innerText = 'Até amanhã!'
+    oneNotice.innerHTML = 'Hoje nosso estabelecimento encontra-se <span class="foco">fechado</span>.'
+    //HOY NO ABRIMOS
+}
