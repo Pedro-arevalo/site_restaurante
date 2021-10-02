61f3d8252a6b6f8c75eebaa0345d1fab92e2a023
Pedro Arevalo <devpedroarevalo@gmail.com>
2021-10-01 23:36:59 -0400

    i programmed the navigation bar with ajax requests... still on it

--- script/script.js
+++ script/script.js
@@ -1,37 +1,58 @@
-var weekDay = new Date().getDay()
-var hour = new Date().getHours()
-
-var oneTitle = document.querySelector('section#two .sectTitle')
-var oneNotice = document.querySelector('.aviso')
-oneTitle.innerText = ''
-oneNotice.innerText = ''
-$('#menu').hide()
-if (weekDay >=1 && weekDay < 6) {
-    if (hour < 14) {
-       oneTitle.innerText = 'Cardápio de hoje:'
-       $('#menu').show()
-        //CARDÁPIO
+   /*
+    var weekDay = new Date().getDay()
+    var hour = new Date().getHours()
+    var oneTitle = document.querySelector('section#two .sectTitle')
+    var oneNotice = document.querySelector('.aviso')
+    oneTitle.innerText = ''
+    oneNotice.innerText = ''
+    $('#foodMenu').hide()
+    if (weekDay >=1 && weekDay < 6) {
+        if (hour < 14) {
+        oneTitle.innerText = 'Cardápio de hoje:'
+        $('#foodMenu').show()
+            //CARDÁPIO
+        } else {
+            oneTitle.innerText = 'Até amanhã!'
+            oneNotice.innerHTML = 'Nosso estabelecimento encontra-se <span class="foco">fechado</span> no momento.'
+            //HASTA MAÑANA
+        }
+    } else if (weekDay == 6) {
+        if (hour < 14) {
+            oneTitle.innerText = 'Cardápio de hoje:'
+            $('#foodMenu').show()
+            //CARDÁPIO
+        } else {
+            oneTitle.innerText = 'Até segunda!'
+            oneNotice.innerHTML = 'Nosso estabelecimento encontra-se <span class="foco">fechado</span> no momento.'
+            //HASTA EL LUNES
+        }
     } else {
         oneTitle.innerText = 'Até amanhã!'
-        oneNotice.innerHTML = 'Nosso estabelecimento encontra-se <span class="foco">fechado</span> no momento.'
-        //HASTA MAÑANA
+        oneNotice.innerHTML = 'Hoje nosso estabelecimento encontra-se <span class="foco">fechado</span>.'
+        //HOY NO ABRIMOS
+    }*/
+
+function navSwitching(navItemSource) {
+    var xhttp = new XMLHttpRequest()
+    xhttp.onload = function() {
+        var container = document.querySelector('div.container')
+        container.innerHTML = xhttp.responseText
     }
-} else if (weekDay == 6) {
-    if (hour < 14) {
-        oneTitle.innerText = 'Cardápio de hoje:'
-        $('#menu').show()
-        //CARDÁPIO
-    } else {
-        oneTitle.innerText = 'Até segunda!'
-        oneNotice.innerHTML = 'Nosso estabelecimento encontra-se <span class="foco">fechado</span> no momento.'
-        //HASTA EL LUNES
-    }
-} else {
-    oneTitle.innerText = 'Até amanhã!'
-    oneNotice.innerHTML = 'Hoje nosso estabelecimento encontra-se <span class="foco">fechado</span>.'
-    //HOY NO ABRIMOS
+    xhttp.open("GET", navItemSource)
+    xhttp.send()
 }
 
-$('nav button').on('click', function() {
-    alert('botão tá funcionando...')
-})
+//defining the source of each navigation item
+src_navMain = 'menuData_ajax/main.html'
+src_navGallery = 'menuData_ajax/gallery.html'
+src_navAboutUs = 'menuData_ajax/about-us.html'
+
+navSwitching(src_navMain)
+
+//adding click events on each navigation item (button)
+var b_navMain = document.getElementById('main')
+var b_navGallery = document.getElementById('gallery')
+var b_navAboutUs = document.getElementById('about-us')
+b_navMain.addEventListener('click', function(){ navSwitching(src_navMain) })
+b_navGallery.addEventListener('click', function(){ navSwitching(src_navGallery) })
+b_navAboutUs.addEventListener('click', function(){ navSwitching(src_navAboutUs) })
