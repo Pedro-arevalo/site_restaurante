034684b0b73a8262355f4f6d0f0ff04cca76933a
Pedro Arevalo <devpedroarevalo@gmail.com>
2021-10-03 04:12:34 -0400

    ajax request for navigation finished, price table added, and layout upgraded

--- script/script.js
+++ script/script.js
@@ -33,13 +33,57 @@
     }*/
 
 function navSwitching(navItemSource) {
+    //the script for the little message section on the main page
+    function mainNotice() {
+        var weekDay = new Date().getDay()
+        var hour = new Date().getHours()
+        var oneTitle = document.querySelector('section#two .sectTitle')
+        var oneNotice = document.querySelector('.aviso')
+        oneTitle.innerText = ''
+        oneNotice.innerText = ''
+        $('#foodMenu').hide()
+        hour = 9;
+        weekDay = 4;
+        if (weekDay >=1 && weekDay < 6) {
+            if (hour < 14) {
+            oneTitle.innerText = 'Cardápio de hoje:'
+            $('#foodMenu').show()
+                //CARDÁPIO
+            } else {
+                oneTitle.innerText = 'Até amanhã!'
+                oneNotice.innerHTML = 'Nosso estabelecimento encontra-se <span class="foco">fechado</span> no momento.'
+                //HASTA MAÑANA
+            }
+        } else if (weekDay == 6) {
+            if (hour < 14) {
+                oneTitle.innerText = 'Cardápio de hoje:'
+                $('#foodMenu').show()
+                //CARDÁPIO
+            } else {
+                oneTitle.innerText = 'Até segunda!'
+                oneNotice.innerHTML = 'Nosso estabelecimento encontra-se <span class="foco">fechado</span> no momento.'
+                //HASTA EL LUNES
+            }
+        } else {
+            oneTitle.innerText = 'Até amanhã!'
+            oneNotice.innerHTML = 'Hoje nosso estabelecimento encontra-se <span class="foco">fechado</span>.'
+            //HOY NO ABRIMOS
+        }
+    }
     var xhttp = new XMLHttpRequest()
     xhttp.onload = function() {
         var container = document.querySelector('div.container')
-        container.innerHTML = xhttp.responseText
+        if (xhttp.status === 200) {
+            container.innerHTML = xhttp.responseText
+        } else {
+            container.innerHTML = '<h2>Oops! Ocorreu um erro inesperado. Tente novamente mais tarde.</h2>'
+        }
+        if (navItemSource == src_navMain) {
+            mainNotice()
+        }
     }
-    xhttp.open("GET", navItemSource)
-    xhttp.send()
+    xhttp.open("GET", navItemSource, true)
+    xhttp.send(null)
 }
 
 //defining the source of each navigation item
